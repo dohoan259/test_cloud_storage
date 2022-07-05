85af52984a22932c2a08c2656f4bf9b950eab555
dohoan259 <dohoan259@gmail.com>
2022-07-05 21:17:26 +0700

    add cloud messenging

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:test_cloud_storage/firebase_options.dart';
 
 import 'home_page.dart';
@@ -8,27 +10,110 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  Future<void> _firebaseMessagingBackgroundHandler(
+      RemoteMessage message) async {
+    // If you're going to use other Firebase services in the background, such as Firestore,
+    // make sure you call `initializeApp` before using other Firebase services.
+    await Firebase.initializeApp();
+
+    print("hoan.dv: Handling a background message: ${message.messageId}");
+  }
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
+      FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+      NotificationSettings settings = await messaging.requestPermission(
+        alert: true,
+        announcement: false,
+        badge: true,
+        carPlay: false,
+        criticalAlert: false,
+        provisional: false,
+        sound: true,
+      );
+
+      if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+        final token = await FirebaseMessaging.instance.getToken();
+
+        //token: dùng để xác định các device khác nhau:
+        print('hoan.dv: token firebase : $token');
+
+        FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+          print('hoan.dv: Got a message whilst in the foreground!');
+          print('hoan.dv: Message data: ${message.data}');
+
+          if (message.notification != null) {
+            print(
+                'hoan.dv: Message also contained a notification: ${message.notification}');
+          }
+
+          // show local notification
+
+          // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+          const AndroidInitializationSettings initializationSettingsAndroid =
+              AndroidInitializationSettings('@drawable/messenger');
+          const InitializationSettings initializationSettings =
+              InitializationSettings(
+            android: initializationSettingsAndroid,
+          );
+
+          FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+              FlutterLocalNotificationsPlugin();
+
+          flutterLocalNotificationsPlugin.initialize(initializationSettings,
+              onSelectNotification: (payload) {
+            if (payload != null) {
+              selectNotification(payload);
+            }
+          });
+
+          // show notification
+          const AndroidNotificationDetails androidPlatformChannelSpecifics =
+              AndroidNotificationDetails('Channel ID', 'Channel name',
+                  channelDescription: 'your channel description',
+                  importance: Importance.max,
+                  priority: Priority.high,
+                  ticker: 'ticker');
+          const NotificationDetails platformChannelSpecifics =
+              NotificationDetails(android: androidPlatformChannelSpecifics);
+          await flutterLocalNotificationsPlugin.show(
+              0, 'plain title', 'plain body', platformChannelSpecifics,
+              payload: 'item x');
+        });
+      }
+    });
+    super.initState();
+  }
+
+  void selectNotification(String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+    // await Navigator.push(
+    //   context,
+    //   MaterialPageRoute<void>(builder: (context) => SecondScreen(payload)),
+    // );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
